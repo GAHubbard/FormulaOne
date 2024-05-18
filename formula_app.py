f13532fd2a35199aecc601bf2d12955710090ee3
GAHubbard <graham.a.hubbard@gmail.com>
2024-05-18 01:02:10 -0500

    Hope this works

--- formula_app.py
+++ formula_app.py
@@ -3,42 +3,53 @@ import json
 import asyncio
 import websockets
 import urllib      
-import time                      
+import time
+import datetime                    
 
 
-def create_url(type: str, url: str, action: str, parameters: dict) -> str:
+def create_url(type: str, action: str, parameters: dict) -> str:
+    url = 'livetiming.formula1.com/signalr'
     connection_type = 'wss' if type == 'websocket' else 'https'
-    parameters_encoded = urllib.parse.urlencode(parameters)
-    url_unparsed = f'{connection_type}://{url}/{action}?{parameters_encoded}'
+    parameters_encoded = urllib.parse.urlencode(parameters) 
+    url_unparsed = f'{connection_type}://{url}/{action}{"?" if parameters_encoded else ""}{parameters_encoded}'
     return url_unparsed
 
 
 def get_handshake() -> requests.Response:
-    raw_url = 'livetiming.formula1.com/signalr'
     get_parameters = {"connectionData": [{"name":"Streaming"}], "clientProtocol": "1.5"}
     get_action = 'negotiate'
     get_connection_type = 'rest'
-    get_url = create_url(get_connection_type, raw_url, get_action, get_parameters)
+    get_url = create_url(get_connection_type, get_action, get_parameters)
     response = requests.get(get_url)
     return response
 
 
 async def handler(url: str, headers: dict, data: json):
-    output_file = open('output.txt', 'a')
-    i = 0
+    output_file = open(f'output-{datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")}.txt', 'a')
     async with websockets.connect(url, extra_headers=headers) as ws:
-        while i < 100:
-            start_time = time.time()
+        while True:
+            # start_time = time.time()
             await ws.send(data)
             response = await ws.recv()
-            time_split = time.time()-start_time
+            # time_split = time.time()-start_time
             if 'R' in json.loads(response):
-                output_file.write(str(time_split) + ' ' + response + '\n')
-            i += 1
+                # response_dict = json.loads(response)
+                # cardata_z = response_dict['R']['CarData.z'] + 'CarData.z.jsonStream' 
+                # await get_car_data(cardata_z, headers=headers)
+                output_file.write(response + '\n')
+                time.sleep(0.5)
+
+
+"""
+async def get_car_data(data_path, headers):
+    connection = 'rest'
+    car_data_url = create_url(connection, data_path, {})
+    response = requests.post(car_data_url, headers)
+    print(response)
+"""
 
 
 async def establish_websocket_session(token: str, cookie: str):
-    raw_url = 'livetiming.formula1.com/signalr'
     # Create websocket headers with cookie
     wss_data = json.dumps({"H": "Streaming", "M": "Subscribe", "A": [["Heartbeat", "CarData.z", "Position.z",
                             "ExtrapolatedClock", "TopThree", "RcmSeries",
@@ -51,7 +62,7 @@ async def establish_websocket_session(token: str, cookie: str):
     wss_action = 'connect'
     wss_connection_type = 'websocket'
     # Create websocket connection url with conneciton token
-    wss_url = create_url(wss_connection_type, raw_url, wss_action, wss_parameters)
+    wss_url = create_url(wss_connection_type, wss_action, wss_parameters)
     await handler(wss_url, wss_headers, wss_data)
 
 
