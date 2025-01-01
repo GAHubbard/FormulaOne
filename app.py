11a64e5cfa9c9ebc8d1a5915221b7bb67acebf55
GAHubbard <32229008+GAHubbard@users.noreply.github.com>
2024-12-31 23:13:27 -0600

    Major changes
    Moved data_handler to new class
    
    Removed loop variable added break instead
    
    Added output_to_file flag

--- app.py
+++ app.py
@@ -2,56 +2,15 @@
 I don't know, I guess some stuff goes here
 """
 
-from f1websocket import F1WebSocket     # Custom F1 WebSocket Package
-from datetime import datetime           # Base
+
 from threading import Thread            # Base
-from contextlib import closing          # Base
-from dateutil import parser             # pip install python-dateutil
-import json                             # Base
-
-def data_handler():
-    """
-    Gets data from F1's Live Timing API endpoint and saves it to a file.
-    :return:
-    """
-
-    # output file saved in YYYY-MM-DD HH-MM-SS.txt in append mode
-    output_file = open(f'output-{datetime.now().strftime("%Y-%m-%d %H-%M-%S")}.txt', 'a', encoding='utf-8')
-
-    session = F1WebSocket()  # create custom web socket
-
-    # Ensure websocket closes in case of errors
-    with closing(session.connection()) as conn:
-
-        conn.send(session.invoke_data)     # Send message to SignalR endpoint to request specific data
-
-        stale_data_count = 0            # Stale data count set to 0
-        previous_heartbeat = None       # Previous Heartbeat timestamp
-        data_handler_loop: bool = True  # Loop control boolean
-
-        # data handler loop
-        while data_handler_loop:
-
-            data = json.loads(conn.recv())  # receive data back from the server and convert to a dictionary
-
-            if 'R' in data:  # If the message received from the server actually has race data in  it
-
-                current_heartbeat = parser.parse(data['R']['Heartbeat']['Utc'])  # Convert heartbeat to datetime obj
-
-                # if previous heartbeat datetime has not been set or earlier in time to the current heart beat
-                if previous_heartbeat is None or current_heartbeat > previous_heartbeat:
-                    stale_data_count = 0                    # reset the stale data counter to 0
-                    previous_heartbeat = current_heartbeat  # update the previous heartbeat datetime
-                    output_file.write(str(data) + '\n')     # output the new data (not stale) to the file
-                else:
-                    stale_data_count += 1                   # add the stale data counter up
-
-                # set the loop boolean to False if the stale data counter gets to 100 or ask the server for more data
-                data_handler_loop = False if stale_data_count >= 100 else conn.send(session.invoke_data)
+from f1_race import F1
 
 
 def main():
-    data_handler_thread = Thread(target=data_handler())
+    race = F1()
+    race.output_to_file = True
+    data_handler_thread = Thread(target=race.session())
     data_handler_thread.start()
     data_handler_thread.join()
 
--- f1_race.py
+++ f1_race.py
@@ -9,6 +9,10 @@ from datetime import datetime
 import utils
 import requests
 from requests import Response
+from f1websocket import F1WebSocket
+from contextlib import closing          # Base
+from dateutil import parser             # pip install python-dateutil
+import json                             # Base
 
 
 class F1:
@@ -21,6 +25,7 @@ class F1:
         self.minute = datetime.now().strftime('%M')
         self.seconds = datetime.now().strftime('%S')
         self.netloc = 'livetiming.formula1.com'
+        self.output_to_file = False
         pass
 
     def get_schedule(self) -> Response:
@@ -32,14 +37,51 @@ class F1:
         return response
 
     def session(self):
-        #do what data handler does?
-        pass
+        """
+        Gets data from F1's Live Timing API endpoint and saves it to a file.
+        :return:
+        """
+
+        # output file saved in YYYY-MM-DD HH-MM-SS.txt in append mode
+        if self.output_to_file:
+            output_file = open(f'output-{datetime.now().strftime("%Y-%m-%d %H-%M-%S")}.txt', 'a', encoding='utf-8')
+
+        websocket = F1WebSocket()  # create custom web socket
+
+        # Ensure websocket closes in case of errors
+        with closing(websocket.connection()) as conn:
+
+            conn.send(websocket.invoke_data)     # Send message to SignalR endpoint to request specific data
+
+            stale_data_count = 0            # Stale data count set to 0
+            previous_heartbeat = None       # Previous Heartbeat timestamp
+
+            # data handler loop
+            while True:
+
+                data = json.loads(conn.recv())  # receive data back from the server and convert to a dictionary
+
+                if 'R' in data:  # If the message received from the server actually has race data in  it
+
+                    current_heartbeat = parser.parse(data['R']['Heartbeat']['Utc'])  # Convert heartbeat to datetime obj
+
+                    # if previous heartbeat datetime has not been set or earlier in time to the current heart beat
+                    if previous_heartbeat is None or current_heartbeat > previous_heartbeat:
+                        stale_data_count = 0                    # reset the stale data counter to 0
+                        previous_heartbeat = current_heartbeat  # update the previous heartbeat datetime
+                        if self.output_to_file:
+                            output_file.write(str(data) + '\n')     # output the new data (not stale) to the file
+                    else:
+                        stale_data_count += 1                   # add the stale data counter up
+
+                    # set the loop boolean to False if the stale data counter gets to 100 or ask the server for more data
+                    if stale_data_count >= 100:
+                        break
+                    else:
+                        conn.send(websocket.invoke_data)
 
     def get_car_telemetry_data(self):
         pass
 
     def get_car_position_data(self):
-        pass
-
-    def output_to_file(self):
         pass
