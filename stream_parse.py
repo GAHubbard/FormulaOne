db3313e8a0272dbaa8bf380fd9c4b51c8cfad0e5
GAHubbard <32229008+GAHubbard@users.noreply.github.com>
2025-03-16 15:51:54 -0500

    Prased JSON streams from AUS Race 3-16

--- stream_parse.py
+++ stream_parse.py
@@ -2,10 +2,10 @@ import json
 import requests
 
 
-response = requests.get('https://livetiming.formula1.com/static/2024/2024-11-23_Las_Vegas_Grand_Prix/2024-11-22_Practice_3/Index.json')
+response = requests.get('https://livetiming.formula1.com/static/2025/2025-03-16_Australian_Grand_Prix/2025-03-16_Race/Index.json')
 stream_data = json.loads(response.content)
 
 for feed in stream_data['Feeds']:
-    output_file = open(f'output\{feed}_jsonstream.txt', 'w')
-    feed_response = requests.get(f'https://livetiming.formula1.com/static/2024/2024-11-23_Las_Vegas_Grand_Prix/2024-11-22_Practice_3/{stream_data['Feeds'][feed]['StreamPath']}')
+    output_file = open(f'jsonstreams_output_files/{feed}_jsonstream.txt', 'w')
+    feed_response = requests.get(f'https://livetiming.formula1.com/static/2025/2025-03-16_Australian_Grand_Prix/2025-03-16_Race/{stream_data['Feeds'][feed]['StreamPath']}')
     output_file.write(feed_response.content.decode('utf-8-sig'))
