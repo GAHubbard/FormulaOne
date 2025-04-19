b7a471820f0d9ebf8a94e094b5248bbabb3594e8
Matthew Raburn <matthew.raburn@gmail.com>
2025-04-19 18:02:47 -0400

    added some commentary that maybe create_url isn't needed

--- dashboard/application/utils.py
+++ dashboard/application/utils.py
@@ -11,6 +11,7 @@ import urllib.parse as url
 def create_url(scheme: str, netloc: str, path: str, params: str = '', query_parameters: dict = {}, fragment: str = "") -> str:
     """
     Returns encoded url with given url connection type, action and parameters
+    !!** THERE IS A CHANCE THAT REQUESTS HANDLES THIS ALREADY SO NO NEED TO DO IT WITH THE F1 API**!!
     """
     query = url.urlencode(query_parameters)                     # encode only the parameter section of the url
     url_tuple = (scheme, netloc, path, params, query, fragment) # create a tuple of all the components
