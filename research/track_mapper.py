45e0cb61790f275516f146ae1e97d558a80d16e7
Matthew Raburn <matthew.raburn@gmail.com>
2025-06-18 01:26:42 -0400

    we have animation of positions over time!

--- research/track_mapper.py
+++ research/track_mapper.py
@@ -4,6 +4,8 @@ File is used to research how F1 live timing actually creates coordinates for the
 
 import json
 from pandas import DataFrame
+import time
+import matplotlib.pyplot as plt
 
 FILE_PATH_TO_TELEMETRY = r"C:\Users\Matth\PycharmProjects\F1\FormulaOne\dashboard\application\position_map_44_2025-06-15-1444.txt"
 
@@ -36,21 +38,62 @@ def get_min_max_x(telemetry_data: DataFrame) -> (DataFrame, DataFrame):
 def get_min_max_y(telemetry_data: DataFrame) -> (DataFrame, DataFrame):
     return telemetry_data['Y'].min(), telemetry_data['Y'].max()
 
+def get_min_max_z(telemetry_data: DataFrame) -> (DataFrame, DataFrame):
+    return telemetry_data['Z'].min(), telemetry_data['Z'].max()
+
+def get_min_max_time(telemetry_data: DataFrame) -> (DataFrame, DataFrame):
+    return telemetry_data['Timestamp'].min(), telemetry_data['Timestamp'].max()
 
 def map_telemetry_data(telemetry_data: DataFrame) -> None:
 
     min_x, max_x = get_min_max_x(telemetry_data)
     min_y, max_y = get_min_max_y(telemetry_data)
-
-
+    min_z, max_z = get_min_max_z(telemetry_data)
+    min_time, max_time = get_min_max_time(telemetry_data)
 
     print(f"X range: {min_x} - {max_x}")
     print(f"Y range: {min_y} - {max_y}")
+    print(f"Z range: {min_z} - {max_z}")
+    print(f"Time range: {min_time} - {max_time}")
 
-    pass
+    plt.figure(figsize=(12, 8))
+
+    x_range = max_x - min_x
+    y_range = max_y - min_y
+    z_range = max_z - min_z
+
+    plt.ion()
+
+    plt.xlim(min_x - round(x_range * .1), max_x + round(x_range * .1))
+    plt.ylim(min_y - round(y_range * .1), max_y + round(y_range * .1))
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.autoscale(False)
+
+    plt.grid(True)
+    plt.xlabel("X Coordinate")
+    plt.ylabel("Y Coordinate")
+    plt.title("Telemetry Grid")
+
+
+    map_x_y = plt.scatter([],[])
+    plt.show()
+    x_so_far = []
+    y_so_far = []
+
+    # plot per record per 1 second
+    for record in telemetry_data.itertuples():
+        x_so_far.append(record.X)
+        y_so_far.append(record.Y)
+        map_x_y.set_offsets(list(zip(x_so_far, y_so_far)))
+        plt.draw()
+        plt.pause(1)
+
+    # loop while preserving real time passing
 
 if __name__ == "__main__":
     telemetry_df = convert_telemetry_file_to_dataframe(FILE_PATH_TO_TELEMETRY)
 
     map_telemetry_data(telemetry_df)
+
+
 
